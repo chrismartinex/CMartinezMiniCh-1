043700bcd60ea93dcfdbb85c083f901dc0c9d1b8
chris martinex <uschrismartinez@gmail.com>
2022-10-18 11:49:20 -0700

    added validation

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿// Chris Martinez
+//09/07/22 
+// Mini Challenge #1 "What is your name?"
+//Peer Review: Jessie - Code looks good and is working.
+// Console.Clear();
+// string name;
+// Console.Write("Buenos Dias!, What is your name:");
+// name = Console.ReadLine();
+// Console.WriteLine("Hola "  + name);
+Console.Clear();
+bool playing = true;
+int wrong = 0;
+bool number;
+string no;
+while(playing)
+{
+    Console.WriteLine("Hello what is your Nombre?<><><><><><> Mine is, HolaBot<><><><><><>.");
+    string MyName = Console.ReadLine();
+            number = Int32.TryParse(MyName, out wrong);
+        if(number == true){
+            Console.WriteLine("please enter a NAME not digits.");
+        }else
+        {
+            Console.WriteLine("Gracias " + MyName + ".");
+        }
+            Console.WriteLine("would you like to play again? type in yes or no");
+            no = Console.ReadLine();
+        if(no == "no")
+        {
+            playing = false;
+        }
+}
